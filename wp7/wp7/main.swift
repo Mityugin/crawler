e953081f396ac31fe1c470621bc5ea2688a455d6
Mityugin <mitkirill@gmail.com>
2016-07-08 00:37:54 +0300

    Add files via upload

--- wp7/wp7/main.swift
+++ wp7/wp7/main.swift
@@ -20,10 +20,11 @@ func matchesForRegexInText(regex: String, text: String) -> [String] {
 
 let domain = "mityugin.com"
 let htp = "http://"
+let searchStr = "15 лет"
+
 let url = htp+domain
 let session = URLSession.shared()
 let request = URLRequest(url: URL(string: url)!)
-
 let task = session.dataTask(with: request, completionHandler: {
     (data, response, error) -> Void in
     
@@ -58,15 +59,41 @@ let task = session.dataTask(with: request, completionHandler: {
             }
         
         }
-        print(pageUrls)
-        pageUrls.write(tofi)
-        //print(matchesForRegexInText(regex: "</\\.*?>(.*?)</\\>", text: myString))
-
+        
+        let lineset = pageUrls.sorted().joined(separator: ",")
+        do {
+            try lineset.write(toFile: "\(domain).links", atomically: true, encoding: usedEncoding)
+        } catch {
+            // failed to write file – bad permissions, bad filename, missing permissions, or more likely it can't be converted to the encoding
+        }
+        
+        // Search for one string in another.
+        var result = myString.range(of: searchStr, options: NSString.CompareOptions.literalSearch, range: myString.startIndex..<myString.endIndex, locale: nil)
+        
+        // See if string was found.
+        if let range = result {
+            
+            // Display range.
+            
+            let start = range.lowerBound
+            let end = myString.index(start, offsetBy: 140)
+            let mySubString = myString.substring(with: start..<end)
+            
+            do {
+                try mySubString.write(toFile: "\(domain).search", atomically: true, encoding: usedEncoding)
+            } catch {
+                // failed to write file – bad permissions, bad filename, missing permissions, or more likely it can't be converted to the encoding
+            }
+  
+            
+        }
     } else {
         print("failed to decode data")
     }
 })
+
+// Running URLSession
 task.resume()
 
-
+// Terminate execution with CTRL+C
 RunLoop.main().run()
